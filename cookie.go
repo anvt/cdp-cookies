6e522d3135dc19ddb5507894ba390b46bc46c1b6
bcmmbaga <bethuelmbaga12@gmail.com>
2019-09-14 12:46:54 +0300

    rm wrong context arg in SetCookies func

--- cookie.go
+++ cookie.go
@@ -7,13 +7,13 @@ import (
 	"github.com/chromedp/chromedp"
 )
 
-// CookieParams represent cookies to be set.
+// CookiesParams represent cookies to be set.
 type CookiesParams struct {
 	Cookies []*network.CookieParam
 }
 
 // SetCookies sets given cookies against the provided context and return chromdedp.Action.
-func (s *CookiesParams) SetCookies(ctx context.Context) chromedp.Action {
+func (s *CookiesParams) SetCookies() chromedp.Action {
 	return chromedp.ActionFunc(func(ctx context.Context) error {
 		err := network.SetCookies(s.Cookies).Do(ctx)
 		if err != nil {
